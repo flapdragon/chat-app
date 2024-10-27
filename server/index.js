6304ae816299086ad2a2bae9537d8f7394ff1f3c
David Dean <vermicious.knid.d@gmail.com>
2024-10-27 08:31:59 -0700

    Initial setup. Adds Socket.io example.

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import path from "path"
+import http from "http"
+import express from "express"
+import { Server } from "socket.io"
+import { fileURLToPath } from "url"
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+const app = express()
+const port = 3300
+
+const server = http.createServer(app)
+
+const io = new Server(server)
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html")
+})
+
+io.on("connection", (socket) => {
+  console.log("a user connected")
+})
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg)
+  })
+})
+
+server.listen(port, () => {
+  console.log(`Chat app listening on *:${port}`)
+})
